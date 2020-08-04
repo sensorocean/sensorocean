e720be56693858720fa2eabbe3ef8ee29ae575f4
sensorocean <67320172+sensorocean@users.noreply.github.com>
2020-08-04 19:48:50 +0300

    Create esp8266_simple.ino

---
+++ examples/gateway/esp8266/esp8266_simple/esp8266_simple.ino
@@ -0,0 +1,41 @@
+#define MY_DEBUG
+#define MY_BAUD_RATE 115200
+
+#define MY_RADIO_RF24
+#define MY_RF24_CHANNEL 124
+
+// WIFI SSID and password
+#define MY_WIFI_SSID "your-wifi-ssid"
+#define MY_WIFI_PASSWORD "your-wifi-password"
+
+// MQTT broker
+#define MY_GATEWAY_MQTT_CLIENT
+#define MY_GATEWAY_ESP8266
+#define MY_CONTROLLER_URL_ADDRESS "mqtt.sensorocean.com"
+#define MY_PORT 1883
+
+#define SENSOROCEAN_GATEWAY_ID "sensorocean-gateway-id"
+#define MY_MQTT_PUBLISH_TOPIC_PREFIX SENSOROCEAN_GATEWAY_ID "-out"
+#define MY_MQTT_SUBSCRIBE_TOPIC_PREFIX SENSOROCEAN_GATEWAY_ID "-in"
+#define MY_MQTT_CLIENT_ID SENSOROCEAN_GATEWAY_ID
+
+#define MY_MQTT_USER "your-sensorocean-username"
+#define MY_MQTT_PASSWORD "your-sensorocean-password"
+
+#include <ESP8266WiFi.h>
+#include <MySensors.h>
+
+void setup()
+{
+	// Setup locally attached sensors
+}
+
+void presentation()
+{
+	// Present locally attached sensors here
+}
+
+void loop()
+{
+	// Send locally attached sensors data here
+}
