0aea070577f7f1a45f6633d656f780b4d05aa186
sensorocean <67320172+sensorocean@users.noreply.github.com>
2021-12-04 09:56:53 +0200

    Update arduino-nano-geiger-counter.ino
    Updated license info

--- examples/node/arduino-nano-geiger-counter/arduino-nano-geiger-counter.ino
+++ examples/node/arduino-nano-geiger-counter/arduino-nano-geiger-counter.ino
@@ -2,6 +2,10 @@
  *  Geiger Counter
  *  Arduino Nano
  *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  version 2 as published by the Free Software Foundation.
+ *
  */
 
 // Enable debug prints to serial monitor
