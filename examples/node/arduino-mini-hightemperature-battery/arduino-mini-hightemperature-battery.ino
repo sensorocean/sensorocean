5626cd0ad4c76c5c635a7d7b19ef2d32ceb4721d
sensorocean <67320172+sensorocean@users.noreply.github.com>
2021-12-04 09:55:29 +0200

    Update arduino-mini-hightemperature-battery.ino
    Added license info

--- examples/node/arduino-mini-hightemperature-battery/arduino-mini-hightemperature-battery.ino
+++ examples/node/arduino-mini-hightemperature-battery/arduino-mini-hightemperature-battery.ino
@@ -3,6 +3,10 @@
  *  HighTemperature Sensor with Battery measurer
  *  Arduino Mini
  *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
  */
 
 // Enable debug prints
